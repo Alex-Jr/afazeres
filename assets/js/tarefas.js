7347e3d509a8e191e0cf6dc05958fadb945f48b3
Alex-Jr <jr.alexmendes@gmail.com>
2019-09-28 10:42:11 -0300

    Setas para mudar a posição

--- assets/js/tarefas.js
+++ assets/js/tarefas.js
@@ -2,18 +2,29 @@ $("ul").on("click", "li", function(){
     $(this).toggleClass("feito")
 })
 
-$("ul").on("click","span", function(event){
+$("ul").on("click",".trash", function(event){
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
     })
     event.stopPropagation();
 })
 
+$("ul").on("click",".up", function(event){
+    index = $(this).parent().index();
+    $('ul').find('li:eq('+(index)+')').insertBefore('li:eq('+(index-1)+')');
+    event.stopPropagation();
+})
+
+$("ul").on("click",".down", function(event){
+    index = $(this).parent().index();
+    $('ul').find('li:eq('+(index+1)+')').insertBefore('li:eq('+index+')');
+    event.stopPropagation();
+})
 
 function adicionar(input){
     if (input.val() != ""){
         input.removeClass("nodata");
-        $("ul").append('<li><span><i class="fa fa-trash"></i></span>'+ input.val()+ '</li>');
+        $("ul").append('<li> <span class="trash"><i class="fa fa-trash"></i></span>'+ input.val()+'<span class="arrow down"><i class="fa fa-angle-down"></i></span><span class="arrow up"><i class="fa fa-angle-up"></i></span></li>');
         input.val("");
         input.attr("placeholder", "Nova tarefa...");
     } else {    
@@ -44,3 +55,4 @@ $("body").keypress(function(event){
     input.fadeIn();
     input.focus();
 })
+
