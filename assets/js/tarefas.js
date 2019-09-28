142448d0601b30701de3ebb70946fd9587bb847c
Alex-Jr <jr.alexmendes@gmail.com>
2019-09-28 08:12:37 -0300

    JS com as funcionalidades básicas

---
+++ assets/js/tarefas.js
@@ -0,0 +1,16 @@
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("feito")
+})
+
+$("ul").on("click","span", function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    })
+    event.stopPropagation();
+})
+
+$("#novo").on("click",function(event){
+    //Adiciona uma nova linha ao UL
+    input = $("input")
+    $("ul").append('<li><span>X</span> '+ input.val()+ '</li>');
+})
